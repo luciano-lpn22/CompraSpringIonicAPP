1b893e43e277d01f2f72ca9484f3849b7f66ac3f
Luciano Nascimento <luciano_lpn22@yahoo.com.br>
2019-08-04 13:23:26 -0300

    Lista de Categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../Services/domain/categoria.service';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -14,12 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public categoriaService:CategoriaService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService
+        .findAll()
+        .subscribe(response=>{
+          console.log(response);
+        },erro=>{
+          console.log("erro")
+        });
   }
 
+  
 }
